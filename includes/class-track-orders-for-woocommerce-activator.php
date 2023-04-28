94f3841e3967cf3b7458f385db08a864839ed644
rishabhmwb <rishabhsingh@makewebbetter.com>
2023-04-28 09:54:49 +0530

    activation and deactivation work is done

--- includes/class-track-orders-for-woocommerce-activator.php
+++ includes/class-track-orders-for-woocommerce-activator.php
@@ -28,17 +28,55 @@ class Track_Orders_For_Woocommerce_Activator {
 	 * @since    1.0.0
 	 */
 	public static function track_orders_for_woocommerce_activate() {
+		$email = get_option( 'admin_email', false );
+		$admin = get_user_by( 'email', $email );
+		$admin_id = $admin->ID;
 
-		$timestamp = get_option( 'wps_tofw_activated_timestamp', 'not_set' );
+		$wps_tofw_tracking = array(
+			'post_author'    => $admin_id,
+			'post_name'      => 'track-your-order',
+			'post_title'     => __( 'Track Order', 'track-orders-for-woocommerce' ),
+			'post_type'      => 'page',
+			'post_status'    => 'publish',
 
-		if ( 'not_set' === $timestamp ) {
+		);
 
-			$current_time = current_time( 'timestamp' );
-
-			$thirty_days = strtotime( '+30 days', $current_time );
-
-			update_option( 'wps_tofw_activated_timestamp', $thirty_days );
+		$page_id = wp_insert_post( $wps_tofw_tracking );
+		$wps_tofw_pages = array();
+		if ( $page_id ) {
+			$wps_tofw_pages['pages']['wps_track_order_page'] = $page_id;
 		}
+
+		$wps_tofw_guest_request_form = array(
+			'post_author'    => $admin_id,
+			'post_name'      => 'guest-track-order-form',
+			'post_title'     => __( 'Track Your Order', 'track-orders-for-woocommerce' ),
+			'post_type'      => 'page',
+			'post_status'    => 'publish',
+
+		);
+
+		$page_id = wp_insert_post( $wps_tofw_guest_request_form );
+
+		if ( $page_id ) {
+			$wps_tofw_pages['pages']['wps_guest_track_order_page'] = $page_id;
+		}
+		$wps_tofw_fed_ex_tracking = array(
+			'post-author'   => $admin_id,
+			'post_name'     => 'track-fedEx-order',
+			'post_title'    => __( 'Shipment Tracking', 'track-orders-for-woocommerce' ),
+			'post_type'     => 'page',
+			'post_status'   => 'publish',
+
+		);
+
+		$page_id = wp_insert_post( $wps_tofw_fed_ex_tracking );
+		if ( $page_id ) {
+			$wps_tofw_pages['pages']['wps_fedex_track_order'] = $page_id;
+		}
+
+		update_option( 'wps_tofw_tracking_page', $wps_tofw_pages );
+
 		wp_clear_scheduled_hook( 'wpswings_tracker_send_event' );
 		wp_schedule_event( time() + 10, apply_filters( 'wpswings_tracker_event_recurrence', 'daily' ), 'wpswings_tracker_send_event' );
 	}
--- includes/class-track-orders-for-woocommerce-deactivator.php
+++ includes/class-track-orders-for-woocommerce-deactivator.php
@@ -29,31 +29,19 @@ class Track_Orders_For_Woocommerce_Deactivator {
 	 * @since 1.0.0
 	 */
 	public static function track_orders_for_woocommerce_deactivate() {
-		if ( get_option( 'tofw_radio_reset_license', false ) ) {
-			$license_key = get_option( 'wps_tofw_license_key', false );
-			$api_params  = array(
-				'slm_action' => 'slm_deactivate',
-				'secret_key' => TRACK_ORDERS_FOR_WOOCOMMERCE_SPECIAL_SECRET_KEY,
-				'license_key' => $license_key,
-				'registered_domain' => $_SERVER['SERVER_NAME'],
-				'item_reference' => urlencode( TRACK_ORDERS_FOR_WOOCOMMERCE_ITEM_REFERENCE ),
-				'product_reference' => 'wpsPK-2965',
-			);
-			$query       = esc_url_raw( add_query_arg( $api_params, TRACK_ORDERS_FOR_WOOCOMMERCE_LICENSE_SERVER_URL ) );
-			$response    = wp_remote_get(
-				$query,
-				array(
-					'timeout' => 20,
-					'sslverify' => false,
-				)
-			);
+		wp_clear_scheduled_hook( 'wps_tofw_daily_notification' );
+		delete_option( 'wps_tofw_warning_notification_message' );
+		delete_option( 'wps_tofw_warning_notification' );
 
-			$license_data = json_decode( wp_remote_retrieve_body( $response ) );
-			if ( isset( $license_data->result ) && 'success' === $license_data->result ) {
-				   delete_option( 'wps_tofw_license_check' );
-				   delete_option( 'wps_tofw_license_key' );
+		$wps_tofw_pages = get_option( 'wps_tofw_tracking_page' );
+
+		if ( isset( $wps_tofw_pages['pages'] ) && ! empty( $wps_tofw_pages['pages'] ) ) {
+			$pages = $wps_tofw_pages['pages'];
+			foreach ( $pages as $page_id ) {
+				wp_delete_post( $page_id, true );
 			}
 		}
+		delete_option( 'wps_tofw_tracking_page' );
 
 	}
 
