cfb3ea6281544f5537304dbafef305dd2c9c8156
rishabhmwb <rishabhsingh@makewebbetter.com>
2023-06-07 14:11:07 +0530

    warning issues fixed

--- template/wps-track-order-myaccount-page-template1.php
+++ template/wps-track-order-myaccount-page-template1.php
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 $allowed = true;
 
 $current_user_id = get_current_user_id();
-
+$wps_tofw_enable_track_order_popup = '';
 if ( true == $allowed ) {
 
 	$check_value = isset( $_POST['woocommerce-process-checkout-nonce'] ) ? sanitize_text_field( wp_unslash( $_POST['woocommerce-process-checkout-nonce'] ) ) : '';
