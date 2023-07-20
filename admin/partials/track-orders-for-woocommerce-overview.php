a97be9a2ba7997e206d7eac1d2844021b75ee507
rishabhmwb <rishabhsingh@makewebbetter.com>
2023-07-20 15:43:21 +0530

    overview page added

--- admin/partials/track-orders-for-woocommerce-overview.php
+++ admin/partials/track-orders-for-woocommerce-overview.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div class="wps-overview__wrapper">
 	<div class="wps-overview__banner">
-		<img src="<?php echo esc_html( TRACK_ORDERS_FOR_WOOCOMMERCE_DIR_URL ); ?>admin/image/banner.jpg" alt="Overview banner image">
+		<img src="<?php echo esc_html( TRACK_ORDERS_FOR_WOOCOMMERCE_DIR_URL ); ?>admin/image/Plugin_banner_image_Track Order_ for_WooCommerce.jpg" alt="Overview banner image">
 	</div>
 	<div class="wps-overview__content">
 		<div class="wps-overview__content-description">
@@ -25,14 +25,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<p>
 				<?php
 				esc_html_e(
-					'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.                '
+					'The "Track Orders for WooCommerce" plugin is a versatile tool that enhances the order-tracking functionality of your store. It provides a visually appealing interface for customers to track their orders, and the unique tracking ID is enough to show the shipping timeline to users. You can also utilize custom order statuses to match specific business requirements. Also, provide Google tracking functionality to your customers.', 'track-orders-for-woocommerce'
 				);
 				?>
 			</p>
 			<h3><?php esc_html_e( 'As a store owner, you get to:', 'track-orders-for-woocommerce' ); ?></h3>
 			<ul class="wps-overview__features">
-				<li><?php esc_html_e( 'Lorem Ipsum is simply dummy text.', 'track-orders-for-woocommerce' ); ?></li>
-				<li><?php esc_html_e( 'Lorem Ipsum is simply dummy text.', 'track-orders-for-woocommerce' ); ?></li>
+				<li><?php esc_html_e( 'Track Order With 3 Different Templates.', 'track-orders-for-woocommerce' ); ?></li>
+				<li><?php esc_html_e( 'Icon-Based Order Status in the Order Table.', 'track-orders-for-woocommerce' ); ?></li>
+				<li><?php esc_html_e( 'Order Export as a CSV File.', 'track-orders-for-woocommerce' ); ?></li>
+				<li><?php esc_html_e( 'Add Custom Order Status.', 'track-orders-for-woocommerce' ); ?></li>
+				<li><?php esc_html_e( 'Third-party shipment tracking (FedEx).', 'track-orders-for-woocommerce' ); ?></li>
 			</ul>
 		</div>
 		<h2> <?php esc_html_e( 'The Free Plugin Benefits', 'track-orders-for-woocommerce' ); ?></h2>
@@ -40,13 +43,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<div class="wps-overview__keywords-item">
 				<div class="wps-overview__keywords-card">
 					<div class="wps-overview__keywords-image">
-						<img src="<?php echo esc_html( TRACK_ORDERS_FOR_WOOCOMMERCE_DIR_URL . 'admin/image/Advanced-report.png' ); ?>" alt="Advanced-report image">
+						<img src="<?php echo esc_html( TRACK_ORDERS_FOR_WOOCOMMERCE_DIR_URL . 'admin/image/OT_track_order_with_3_different_templates.svg' ); ?>" alt="Advanced-report image">
 					</div>
 					<div class="wps-overview__keywords-text">
-						<h3 class="wps-overview__keywords-heading"><?php echo esc_html_e( ' Lorem Ipsum ', 'track-orders-for-woocommerce' ); ?></h3>
+						<h3 class="wps-overview__keywords-heading"><?php echo esc_html_e( ' Three Different Order Tracking Templates ', 'track-orders-for-woocommerce' ); ?></h3>
 						<p class="wps-overview__keywords-description">
 							<?php
-							esc_html_e( 'Lorem Ipsum ', 'track-orders-for-woocommerce' );
+							esc_html_e( 'Make order tracking more engaging with various templates that elevate the design of your tracking interface. Customers will stay informed and entertained while following their shipment.  ', 'track-orders-for-woocommerce' );
 							?>
 						</p>
 					</div>
@@ -55,13 +58,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<div class="wps-overview__keywords-item">
 				<div class="wps-overview__keywords-card">
 					<div class="wps-overview__keywords-image">
-						<img src="<?php echo esc_html( TRACK_ORDERS_FOR_WOOCOMMERCE_DIR_URL . 'admin/image/Workflow.png' ); ?>" alt="Workflow image">
+						<img src="<?php echo esc_html( TRACK_ORDERS_FOR_WOOCOMMERCE_DIR_URL . 'admin/image/OT_Show_Icon_Instead_of_Text.svg' ); ?>" alt="Workflow image">
 					</div>
 					<div class="wps-overview__keywords-text">
-						<h3 class="wps-overview__keywords-heading"><?php echo esc_html_e( ' Lorem Ipsum ', 'track-orders-for-woocommerce' ); ?></h3>
+						<h3 class="wps-overview__keywords-heading"><?php echo esc_html_e( ' Icon-Based Order Status in the Order Table ', 'track-orders-for-woocommerce' ); ?></h3>
 						<p class="wps-overview__keywords-description">
 							<?php
-							esc_html_e( 'Lorem Ipsum ', 'track-orders-for-woocommerce' );
+							esc_html_e( 'The order details are updated in a tabular format, offering a standard & organized view. You can customize the order status text and replace it with brand-uniform icons for faster visual input. ', 'track-orders-for-woocommerce' );
 							?>
 						</p>
 					</div>
@@ -70,13 +73,43 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<div class="wps-overview__keywords-item">
 				<div class="wps-overview__keywords-card">
 					<div class="wps-overview__keywords-image">
-						<img src="<?php echo esc_html( TRACK_ORDERS_FOR_WOOCOMMERCE_DIR_URL . 'admin/image/Variable-product.png' ); ?>" alt="Variable product image">
+						<img src="<?php echo esc_html( TRACK_ORDERS_FOR_WOOCOMMERCE_DIR_URL . 'admin/image/OT_Order_Export_as_Csv_File.svg' ); ?>" alt="Variable product image">
 					</div>
 					<div class="wps-overview__keywords-text">
-						<h3 class="wps-overview__keywords-heading"><?php echo esc_html_e( ' Lorem Ipsum ', 'track-orders-for-woocommerce' ); ?></h3>
+						<h3 class="wps-overview__keywords-heading"><?php echo esc_html_e( ' Order Export as a CSV File ', 'track-orders-for-woocommerce' ); ?></h3>
 						<p class="wps-overview__keywords-description">
 							<?php
-							esc_html_e( 'Lorem Ipsum ', 'track-orders-for-woocommerce' );
+							esc_html_e( 'Your customers & guest users can export WooCommerce order details in a CSV file and easily examine order history. This user-friendly feature lets users gather key order details. ', 'track-orders-for-woocommerce' );
+							?>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="wps-overview__keywords-item">
+				<div class="wps-overview__keywords-card">
+					<div class="wps-overview__keywords-image">
+						<img src="<?php echo esc_html( TRACK_ORDERS_FOR_WOOCOMMERCE_DIR_URL . 'admin/image/OT_Third-party_Shipment_Tracking.svg' ); ?>" alt="Variable product image">
+					</div>
+					<div class="wps-overview__keywords-text">
+						<h3 class="wps-overview__keywords-heading"><?php echo esc_html_e( ' Third-party shipment tracking (FedEx) ', 'track-orders-for-woocommerce' ); ?></h3>
+						<p class="wps-overview__keywords-description">
+							<?php
+							esc_html_e( 'Customers can enter their FedEx tracking number or other relevant shipment details and use the FedEx tracking system to retrieve real-time updates on the packages location and status. ', 'track-orders-for-woocommerce' );
+							?>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="wps-overview__keywords-item">
+				<div class="wps-overview__keywords-card">
+					<div class="wps-overview__keywords-image">
+						<img src="<?php echo esc_html( TRACK_ORDERS_FOR_WOOCOMMERCE_DIR_URL . 'admin/image/OT_Create_and_Use_Own_Custom_Order_Status.svg' ); ?>" alt="Variable product image">
+					</div>
+					<div class="wps-overview__keywords-text">
+						<h3 class="wps-overview__keywords-heading"><?php echo esc_html_e( ' Add Custom Order Status ', 'track-orders-for-woocommerce' ); ?></h3>
+						<p class="wps-overview__keywords-description">
+							<?php
+							esc_html_e( 'Create and assign unique labels to orders based on your specific business requirements. By adding custom order statuses, you gain greater flexibility and control over order management. ', 'track-orders-for-woocommerce' );
 							?>
 						</p>
 					</div>
